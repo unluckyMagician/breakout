bbe43dc3d1ca0cad1830683b46e2c4d3f7e03460
Ian Parker <IanPark@hpsvikings.org>
2016-05-16 14:45:26 -0400

    Commiting breakthrough changes...

--- js/game.js
+++ js/game.js
@@ -6,13 +6,9 @@ Breakout.Preloader.prototype = {
 
 	preload: function () {
 		this.load.path = 'assets/'
-		this.load.image('bg', 'bg.png');
-		this.load.image('paddle', 'paddle.png');
-		this.load.image('ball', 'ball.png');
-		this.load.image('wall-left', 'walls-vertical-left.png');
-		this.load.image('wall-right', 'walls-vertical-right.png');
-		this.load.image('ceiling', 'walls-ceiling.png');
-		this.load.physics('paddle-physics', 'physics/paddle-physics.json');
+		this.load.image('level1-bg', 'level_1_bg.png');
+		this.load.image('paddle-med', 'paddle_med.png');
+		this.load.image('ball-small', 'ball_small.png');
 	},
 
 	create: function () {
@@ -22,51 +18,84 @@ Breakout.Preloader.prototype = {
 };
 
 Breakout.Level1 = function (game) {
-	this.paddle;
-	this.cursors;
-	this.walls;
+	this.ball_small;
+	this.ball_large;
+	this.paddle_med;
+
+	this.ballOnPaddle = true;
+
 };
 
 Breakout.Level1.prototype = {
 
+
 	create: function () {
-		this.physics.startSystem(Phaser.Physics.P2JS);
-		this.add.sprite(0, 0, 'bg');
+		this.add.sprite(0, 0, 'level1-bg');
+		this.physics.startSystem(Phaser.Physics.ARCADE);
+		this.physics.arcade.checkCollision.down = false;
 
-		this.walls = this.add.group();
-		this.walls.enableBody = true;
-		var wall_left = this.walls.create(0, 0, 'wall-left');
-		var wall_right = this.walls.create(this.world.width - 16, 0, 'wall-right');
-		wall_left.body.immovable = true;
+		this.paddle_med = this.add.sprite(this.world.centerX, this.world.height - 64, 'paddle-med');
+		this.paddle_med.anchor.setTo(0.5, 0.5);
+		this.physics.arcade.enable(this.paddle_med);
+		this.paddle_med.body.immovable = true;
+		this.paddle_med.body.collideWorldBounds = true;
 
-		this.paddle = this.add.sprite(this.world.centerX - 32, this.world.height - 64, 'paddle');
-		this.physics.p2.enable(this.paddle);
-		this.paddle.body.kinematic = true;
-		this.paddle.body.collideWorldBounds = true;
+		this.ball_small = this.add.sprite(this.paddle_med.x, this.paddle_med.y - 8, 'ball-small');
+		this.ball_small.anchor.set(0.5);
+		this.physics.arcade.enable(this.ball_small);
+		this.ball_small.checkWorldBounds = true;
+		this.ball_small.body.collideWorldBounds = true;
+		this.ball_small.body.bounce.x = 1;
+		this.ball_small.body.bounce.y = 1;
 
-		this.cursors = this.input.keyboard.createCursorKeys();
+		game.input.onDown.add(this.launchBall, this);
 
 	},
 
 	update: function () {
-		this.physics.arcade.collide(this.walls, this.paddle);
-		this.updatePaddle();
+		this.physics.arcade.collide(this.ball_small, this.paddle_med, this.ballHitPaddle, null, this);
+		this.paddle_med.x = this.input.mousePointer.x - 24;
+
+		if (this.ballOnPaddle) {
+			this.ball_small.x = this.paddle_med.x + 4;
+		}
 	},
 
-	updatePaddle: function () {
-
-		if (this.cursors.left.isDown) {
-			this.paddle.body.velocity.x = -400;
-		} else if (this.cursors.right.isDown) {
-			this.paddle.body.velocity.x = 400;
-		} else {
-			this.paddle.body.velocity.x = 0;
+	launchBall: function () {
+		if (this.ballOnPaddle) {
+			this.ballOnPaddle = false;
+			this.ball_small.body.velocity.y = -300;
+			this.ball_small.body.velocity.x = 75;
 		}
+	},
+
+	ballHitPaddle: function () {
+
+		var diff = 0;
+	    if (this.ball_small.x < this.paddle_med.x)
+	    {
+	        //  Ball is on the left-hand side of the paddle
+	        diff = this.paddle_med.x - this.ball_small.x;
+	        this.ball_small.body.velocity.x = (-10 * diff);
+	    }
+	    else if (this.ball_small.x > this.paddle_med.x)
+	    {
+	        //  Ball is on the right-hand side of the paddle
+	        diff = this.ball_small.x -this.paddle_med.x;
+	        this.ball_small.body.velocity.x = (10 * diff);
+	    }
+	    else
+	    {
+	        //  Ball is perfectly in the middle
+	        //  Add a little random X to stop it bouncing straight up!
+	        this.ball_small.body.velocity.x = 2 + Math.random() * 8;
+	    }
 	}
+
 };
 
 
-var game = new Phaser.Game(592, 640, Phaser.AUTO, 'game-canvas');
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game-canvas');
 
 game.state.add('Breakout.Preloader', Breakout.Preloader);
 game.state.add('Breakout.Level1', Breakout.Level1);
